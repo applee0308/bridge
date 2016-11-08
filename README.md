c504b3b8117ba7ddfa930c71da9aa913787aaaa7
xincheng <445798337@qq.com>
2016-11-08 14:41:38 -0800

    Add some to README

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # bridge
+
+ADOS[广告投放系统]与Portal[广告平台]
+
+将生成的广告[广告资源来源于CND]渲染值Portal。
+
+###测试
+
+yarn add
+
+yarn run dev
---
+++ dom-form.js
@@ -0,0 +1,3 @@
+"use strict";
+
+var str = "<form id=\"f\">\n        <input type=\"text\" name=\"country\" id=\"country\">\n        <input type=\"text\" name=\"province\" id=\"province\">\n        <input type=\"text\" name=\"city\" id=\"city\">\n    </form>\n    <button id=\"tt\">提交</button>";
